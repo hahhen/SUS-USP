7e5364cb08bd24215c05a6280c411b4ac871b6d6
Arthur Santana <90651161+arthursantana-dev@users.noreply.github.com>
2025-08-19 21:26:43 -0300

    pilha: métodos dinâmicos

---
+++ pilha/pilha.c
@@ -0,0 +1,129 @@
+#include "pilha.h"
+#include "../no/no.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+struct pilha
+{
+    int tamanho;
+    NO *topo;
+};
+
+PILHA *pilha_criar()
+{
+
+    PILHA *p = (PILHA *)malloc(sizeof(PILHA));
+
+    if (p != NULL)
+    {
+        p->tamanho = 0;
+        p->topo = NULL;
+
+    }
+
+    return p;
+}
+
+bool pilha_vazia(PILHA *p)
+{
+    if (p != NULL)
+    {
+        return p->tamanho == 0;
+    }
+
+    return false;
+}
+
+bool pilha_cheia(PILHA *p)
+{
+    if (p != NULL)
+    {
+        
+        NO* tentativa = no_criar(NULL, NULL);
+
+        if(tentativa == NULL){
+            return true;
+        }
+
+        free(tentativa);
+        return false;
+
+    }
+
+    return false;
+}
+
+int pilha_tamanho(PILHA *p)
+{
+
+    if (p != NULL)
+    {
+        return p->tamanho;
+    }
+
+    return -1;
+}
+
+bool pilha_empilhar(PILHA *p, void* valor)
+{
+
+    NO* no = no_criar(valor, NULL);
+
+    if(no == NULL) return false;
+
+    if ((p != NULL) && (!pilha_cheia(p)))
+    {
+
+        no_set_anterior(no, p->topo);
+
+        p->topo = no;
+
+        p->tamanho++;
+
+        return true;
+    }
+
+    return false;
+}
+
+void *pilha_desempilhar(PILHA *p)
+{
+    NO *no;
+    if ((p != NULL) && (!pilha_vazia(p)))
+    {
+
+        no = pilha_topo(p);
+
+        void* no_valor = no_get_valor(no);
+
+        p->topo = no_get_anterior(no);
+
+        no_remover(&no);
+
+        p->tamanho--;
+
+        return no_valor;
+    }
+
+    return NULL;
+}
+
+NO *pilha_topo(PILHA *p)
+{
+    if (p != NULL && p->tamanho > 0)
+    {
+        return p->topo;
+    }
+
+    return NULL;
+}
+
+void pilha_apagar(PILHA **p)
+{
+    if (*p != NULL)
+    {
+        free(*p);
+        *p = NULL;
+    }
+}
+
---
+++ pilha/pilha.h
@@ -0,0 +1,19 @@
+#ifndef _PILHA_H
+#define _PILHA_H
+
+  #include "../no/no.h"
+  #include <stdbool.h>
+  #include <string.h>
+
+  typedef struct pilha PILHA;
+
+  PILHA* pilha_criar(void);
+  void pilha_apagar(PILHA** pilha);
+  bool pilha_vazia(PILHA* pilha);
+  bool pilha_cheia(PILHA* pilha);
+  int pilha_tamanho(PILHA* pilha);
+  NO* pilha_topo(PILHA* pilha);
+  bool pilha_empilhar(PILHA* pilha, void* valor);
+  void* pilha_desempilhar(PILHA* pilha);
+
+#endif 
