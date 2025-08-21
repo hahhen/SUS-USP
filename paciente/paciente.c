0cd402d46ac27cac462f735f6e618db51021fcbc
Arthur Santana <90651161+arthursantana-dev@users.noreply.github.com>
2025-08-21 10:51:38 -0300

    Paciente: métodos iniciais + histórico

---
+++ paciente/paciente.c
@@ -0,0 +1,83 @@
+#include "paciente.h"
+#include "../historico/historico.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Paciente
+{
+    char *nome;
+    int *id;
+    HISTORICO *historico;
+};
+
+PACIENTE *paciente_criar(char *nome, int *id)
+{
+    PACIENTE *paciente = (PACIENTE *)malloc(sizeof(PACIENTE));
+    HISTORICO *historico = historico_criar();
+    if (paciente != NULL && historico != NULL)
+    {
+        paciente->nome = nome;
+        paciente->id = id;
+        paciente->historico = historico;
+
+        return paciente;
+    }
+    return NULL;
+}
+
+char *paciente_get_nome(PACIENTE *paciente)
+{
+    if (paciente == NULL)
+        return NULL;
+
+    return paciente->nome;
+}
+
+int *paciente_get_id(PACIENTE *paciente)
+{
+    if (paciente == NULL)
+        return NULL;
+
+    return paciente->id;
+}
+
+bool paciente_adicionar_procedimento(PACIENTE *paciente, char *procedimento)
+{
+    if (paciente == NULL || strlen(procedimento) == 0 || paciente->historico == NULL)
+        return false;
+
+    historico_inserir(paciente->historico, procedimento);
+    return true;
+}
+
+bool paciente_remover_procedimento(PACIENTE *paciente)
+{
+    if (paciente == NULL || paciente->historico == NULL)
+        return false;
+
+    return historico_remover(paciente->historico);
+}
+
+char *paciente_listar_procedimentos(PACIENTE *paciente)
+{
+    if (paciente == NULL || paciente->historico == NULL)
+        return NULL;
+
+        // printf("paciente_listar_procedimentos: debug\n");
+    char* res = historico_listar(paciente->historico);
+
+    return res;
+}
+
+void paciente_remover(PACIENTE **paciente)
+{
+    if (*paciente == NULL)
+        return;
+
+    historico_apagar(&(*paciente)->historico);
+    free((*paciente)->nome);
+    free((*paciente)->id);
+    free(*paciente);
+    *paciente = NULL;
+}
---
+++ paciente/paciente.h
@@ -0,0 +1,22 @@
+#ifndef PACIENTE_H
+#define PACIENTE_H
+
+#include <stdbool.h>
+
+typedef struct Paciente PACIENTE;
+
+PACIENTE* paciente_criar(char *nome, int* id);
+
+char *paciente_get_nome(PACIENTE *paciente);
+
+int *paciente_get_id(PACIENTE *paciente);
+
+bool paciente_adicionar_procedimento(PACIENTE *paciente, char *procedimento);
+
+bool paciente_remover_procedimento(PACIENTE *paciente);
+
+char *paciente_listar_procedimentos(PACIENTE *paciente);
+
+void paciente_remover(PACIENTE **paciente);
+
+#endif
