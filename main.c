31a9de357517a458310df2e9f03f5d97eead5b21
Arthur Santana <90651161+arthursantana-dev@users.noreply.github.com>
2025-08-21 10:53:00 -0300

    Main: testes com paciente e histórico

--- main.c
+++ main.c
@@ -1,20 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pilha/pilha.h"
+#include "historico/historico.h"
+#include "paciente/paciente.h"
 
-int main(){
+int main()
+{
 
-    PILHA* p = pilha_criar();
+    PILHA *p = pilha_criar();
+    // HISTORICO *historico = historico_criar();
 
-    char* a = "1";
-    char* b = "2";
+    char *a = "fulano";
+    char *b = "ciclano";
+
+    int *ia = (int*)malloc(sizeof(int));
+    int *ib = (int*)malloc(sizeof(int));
+
+    *ia = 20;
+    *ib = 32;
+
+    PACIENTE* pa = paciente_criar(a, ia);
+    PACIENTE* pb = paciente_criar(b, ib);    
 
     pilha_empilhar(p, a);
-
     pilha_empilhar(p, b);
-
     pilha_desempilhar(p);
 
-    printf("%s", no_get_valor(pilha_topo(p)));
+    char* p1 = "teste1";
+    char* p2 = "teste2";
+    char* p3 = "teste3";
+
+    paciente_adicionar_procedimento(pa, p1);
+    paciente_adicionar_procedimento(pa, p2);
+    paciente_adicionar_procedimento(pa, p3);
+
+    paciente_remover_procedimento(pa);
+
+    printf("%s\n", p3);
+
+    printf("%s", paciente_listar_procedimentos(pa));
+    printf("%s", paciente_get_nome(pa));
+    printf("%d", *paciente_get_id(pa));
+
+    // pilha_apagar(&p);
+
+    // historico_inserir(historico, a);
+    // historico_inserir(historico, b);
+    // historico_inserir(historico, b);
+
+    // historico_remover(historico);
+
+    // printf("%s", no_get_valor(pilha_topo(p)));
+    // char *d = historico_listar(historico);
+    // printf("%s", d);
 
     return 0;
 }
